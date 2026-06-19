eb0ff1cb8fa193cb09aa4835ce8c26b53e6b41a7
RX02 <per.a.frivik@gmail.com>
2026-06-19 19:08:30 +0200

    removed reset_cmd_vel() in get_cmd_vel()

--- include/robot/command_egress.hpp
+++ include/robot/command_egress.hpp
@@ -2,6 +2,9 @@
 #define A2_BRIDGE_ROBOT_CMD_EGRESS_H_
 
 #include <unitree/robot/a2/sport/sport_client.hpp>
+#include <geometry_msgs/msg/twist_stamped.hpp>
+#include <std_msgs/msg/float32.hpp>
+#include <std_msgs/msg/int32.hpp>
 #include "a2_utils/safe_velocity_ros_interface.hpp"
 #include "common/egress.hpp"
 
@@ -23,8 +26,12 @@ protected:
   void onControl(utils::OpMode mode, bool mode_changed, std::array<float, 3> vel) override;
 
 private:
-private:
   unitree::robot::a2::SportClient sport_client_;
+  rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr debug_vel_pub_;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr debug_hz_pub_;
+  // 0=ESTOP 1=STAND_DOWN 2=STAND_UP 3=BALANCE_STAND 4=VELOCITY_MOVE 5=FREE
+  rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr debug_mode_pub_;
+  rclcpp::Time last_vel_time_;
 };
 
 }  // namespace bridge
