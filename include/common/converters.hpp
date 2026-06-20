8cd51d660ccf99612726e12ad96a92b42ca2d23a
Per Frivik <94360401+PerFrivik@users.noreply.github.com>
2026-06-20 14:54:39 +0200

    dlio works in sim (#8)

--- include/common/converters.hpp
+++ include/common/converters.hpp
@@ -54,6 +54,62 @@ inline sensor_msgs::msg::Imu imu(const LowState_t& msg,
   return ros_msg;
 }
 
+// Body IMU rotated to the front lidar IMU frame + rigid-body accel correction.
+// R_base_to_lidar = [[0,0,1],[1,0,0],[0,1,0]], so R^T * v = [v.y, v.z, v.x].
+// Accelerometer: a_at_r = a_body + ω×(ω×r) + α×r, then rotated by R^T.
+template <typename LowState_t>
+inline sensor_msgs::msg::Imu front_lidar_imu(const LowState_t& msg,
+                                              const builtin_interfaces::msg::Time& stamp,
+                                              float prev_gyro[3],
+                                              rclcpp::Time& prev_time,
+                                              const rclcpp::Time& cur_time) {
+  static constexpr float r[3] = {0.34629f, -0.00914f, 0.04218f};
+
+  float gx = msg.imu_state().gyroscope()[0];
+  float gy = msg.imu_state().gyroscope()[1];
+  float gz = msg.imu_state().gyroscope()[2];
+
+  float dt = static_cast<float>((cur_time - prev_time).seconds());
+  float alpha[3] = {0.f, 0.f, 0.f};
+  if (dt > 0.f && dt < 0.1f) {
+    alpha[0] = (gx - prev_gyro[0]) / dt;
+    alpha[1] = (gy - prev_gyro[1]) / dt;
+    alpha[2] = (gz - prev_gyro[2]) / dt;
+  }
+  prev_gyro[0] = gx; prev_gyro[1] = gy; prev_gyro[2] = gz;
+  prev_time = cur_time;
+
+  float wxr[3] = { gy*r[2] - gz*r[1], gz*r[0] - gx*r[2], gx*r[1] - gy*r[0] };
+  float cent[3] = { gy*wxr[2] - gz*wxr[1], gz*wxr[0] - gx*wxr[2], gx*wxr[1] - gy*wxr[0] };
+  float tang[3] = { alpha[1]*r[2] - alpha[2]*r[1],
+                     alpha[2]*r[0] - alpha[0]*r[2],
+                     alpha[0]*r[1] - alpha[1]*r[0] };
+
+  float ab[3] = { msg.imu_state().accelerometer()[0] + cent[0] + tang[0],
+                   msg.imu_state().accelerometer()[1] + cent[1] + tang[1],
+                   msg.imu_state().accelerometer()[2] + cent[2] + tang[2] };
+
+  float qw = msg.imu_state().quaternion()[0];
+  float qx = msg.imu_state().quaternion()[1];
+  float qy = msg.imu_state().quaternion()[2];
+  float qz = msg.imu_state().quaternion()[3];
+
+  sensor_msgs::msg::Imu ros_msg;
+  ros_msg.header.stamp = stamp;
+  ros_msg.header.frame_id = "front_lidar_imu_link";
+  ros_msg.angular_velocity.x = gy;
+  ros_msg.angular_velocity.y = gz;
+  ros_msg.angular_velocity.z = gx;
+  ros_msg.linear_acceleration.x = ab[1];
+  ros_msg.linear_acceleration.y = ab[2];
+  ros_msg.linear_acceleration.z = ab[0];
+  ros_msg.orientation.w = 0.5f * (qw - qx - qy - qz);
+  ros_msg.orientation.x = 0.5f * (qw + qx + qy - qz);
+  ros_msg.orientation.y = 0.5f * (qw - qx + qy + qz);
+  ros_msg.orientation.z = 0.5f * (qw + qx - qy + qz);
+  return ros_msg;
+}
+
 inline std_msgs::msg::UInt8 sport_mode(const unitree_go::msg::dds_::SportModeState_& msg) {
   std_msgs::msg::UInt8 ros_msg;
   ros_msg.data = static_cast<uint8_t>(msg.mode());
